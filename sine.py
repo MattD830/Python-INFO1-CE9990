c367e8c9a4234773db638022dfb6e67a4f1d24ac
MattD830 <denglermatt@yahoo.com>
2017-07-06 02:49:52 -0400

    Create sine.py

---
+++ sine.py
@@ -0,0 +1,7 @@
+import sys
+import math
+
+a = str(int(( - math.cos(math.pi) ) - (- math.cos(0) )))
+print("The area is always two ("+a+") so there's no reason to actually calculate it.")
+
+sys.exit(0)
